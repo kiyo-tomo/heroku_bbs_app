b775e11ab9cae150ce94b234c704ef5e3b0bc73f
vagrant <vagrant@homestead>
2019-05-31 16:39:19 +0000

    update

---
+++ database/migrations/2019_05_02_162707_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_name', 25)->default('名無しさん');
+            $table->unsignedInteger('user_id');
+            $table->string('title', 50);
+            $table->text('body');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
